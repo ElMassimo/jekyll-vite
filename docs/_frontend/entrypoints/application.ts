ab0806e587128a65a0a30533031b909bb08208bd
Maximo Mussini <maximomussini@gmail.com>
2021-07-05 16:49:30 -0300

    docs: Debug netlify cache

--- docs/_frontend/entrypoints/application.ts
+++ docs/_frontend/entrypoints/application.ts
@@ -3,8 +3,14 @@ import 'virtual:windi-devtools'
 import '~/styles/main.scss'
 
 import { createApp } from 'vue'
+import { setupPrefetch } from '~/logic/prefetch'
 import NavBarActions from '~/components/NavBarActions.vue'
 
 createApp(NavBarActions).mount('#nav-bar-actions')
 
-console.log('Vite ⚡️ Ruby')
+console.log('Powered by Vite ⚡️ Jekyll')
+console.log('https://github.com/ElMassimo/jekyll-vite')
+
+document.addEventListener('DOMContentLoaded', () => {
+  setupPrefetch()
+})
---
+++ docs/_frontend/logic/prefetch.ts
@@ -0,0 +1,24 @@
+const toPrefetch = new Set()
+
+const sameDomain = (el: HTMLAnchorElement) =>
+  el.hostname === location.hostname && el.protocol === location.protocol
+
+const prefetchHref = ({ type, target: el }: Event) => {
+  const { href: url } = el as HTMLAnchorElement
+  if (!url || toPrefetch.has(url)) return
+
+  toPrefetch.add(url)
+  el?.removeEventListener(type, prefetchHref)
+
+  fetch(url, { credentials: 'include' })
+}
+
+export function setupPrefetch () {
+  Array.from(document.querySelectorAll<HTMLAnchorElement>('a[href]'))
+    .filter(sameDomain)
+    .forEach((el) => {
+      ['touchstart', 'mouseover'].forEach((eventName) => {
+        el.addEventListener(eventName, prefetchHref, { passive: true })
+      })
+    })
+}
